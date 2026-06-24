b45cc9420e1513448f1d39540421ed798ce7a2b2
Anushk Dua <153355489+anushk7777@users.noreply.github.com>
2026-06-25 01:01:56 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Sparkles } from 'lucide-react';
 import { supabase } from '../lib/supabase';
 import { useStore } from '../store/useStore';
 
